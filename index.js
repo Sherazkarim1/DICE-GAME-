61de4ad125a2ded61d784a0addc5ae63c5d8bd88
sheraz <sherazkarim12@gmail.com>
2024-02-08 08:34:11 +0500

    push

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-// function getRandom(){
-//     var RandomNumber1 = Math.floor(Math.random() * 6) + 1;
-//     var RandomNumber2 = Math.floor(Math.random() * 6) + 1;
-//       if(RandomNumber1 == 1) document.write('<img src="dice1.png">');
-//       else if(RandomNumber1 == 2)  document.write('<img src="dice2.png">');
-//       else if(RandomNumber1 == 3)  document.write('<img src="dice3.png">');
-//         else if(RandomNumber1 == 4)  document.write('<img src="dice4.png">');
-//           else if(RandomNumber1 == 5)  document.write('<img src="dice5.png">');
-    
-//                       else  document.write('<img src="dice6.png">');
-    
-//     }
-alert ("perfectly working");
+var randomNumberGen = Math.floor(math.random() *6)
