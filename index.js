313d7a599115561eeae424ccc6f66cbb6438e8a4
sheraz <sherazkarim12@gmail.com>
2024-02-13 14:59:16 +0500

    git commit

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 var randomNumber1 = Math.floor(math.random() *6) + 1 ;     
 var randomDiceImage = "dice" + randomNumber1 + ".png"
+var randomDiceSource = "images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img") [0];
+image1.setAttribute("src", randomDiceSource);
