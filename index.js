cc413f1da97372b981b0062751697a22af1e102d
sheraz <sherazkarim12@gmail.com>
2024-02-13 17:31:51 +0500

    change commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var randomNumber1 = Math.floor(math.random() *6) + 1 ;     
-var randomDiceImage = "dice" + randomNumber1 + ".png"
-var randomDiceSource = "images/" + randomDiceImage;
-var image1 = document.querySelectorAll("img") [0];
-image1.setAttribute("src", randomDiceSource);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
