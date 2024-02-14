8f9d8269ca158cc0b03cef6435cab829da127896
sheraz <sherazkarim12@gmail.com>
2024-02-14 20:13:16 +0500

    commit

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; // Remove the extra space
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//if player 1 wins 
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "player 1 wins!"
+}
+
+//if player 2 wins
+
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "player 2 wins!"
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
