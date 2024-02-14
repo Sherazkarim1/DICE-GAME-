866cda5f65a4db353505d75705810cb0aaa64b2d
sheraz <sherazkarim12@gmail.com>
2024-02-14 20:00:19 +0500

    commit

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage = "dice" + randomNumber1 + ".png";
 var randomImageSource = "images/" + randomDiceImage;
-var image1 = document.querySelectorAll("img")[0];
+var image1 = document.querySelectorAll("img")[0]; // Access the second image element
 image1.setAttribute("src", randomImageSource);
 
-var randomNumber2 = Math.floor(Math.random()* 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice " + randomNumber2 + ".png"
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"; // Remove the extra space
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
- 
